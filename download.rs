55f7534f1a7091d872a07f9cc51b85292b9c82da
Damjan Postolovski <damjan.postolovski@goeverbright.com>
2025-08-04 17:30:25 -0400

    use curl to download lib

--- download.rs
+++ download.rs
@@ -1,3 +1,4 @@
+use std::{env, io};
 use std::error::Error;
 use std::fs::File;
 use std::io::Cursor;
@@ -7,21 +8,69 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::time::Duration;
 use tempfile::tempdir;
+use ureq::{Agent, Proxy};
+use ureq::tls::{TlsConfig, TlsProvider};
 use zip_extract::extract;
+
+fn download_file_curl<T: AsRef<Path>>(url: &str, target_dir: T) -> Result<(), Box<dyn Error>> {
+    // Not using `CARGO_CFG_TARGET_OS` because of the possibility of
+    // cross-compilation. When targeting `x86_64-pc-windows-gnu` on Linux for
+    // example, `cfg!()` in the build script still reports `target_os =
+    // "linux"`, which is desirable.
+    let curl_bin_name = if cfg!(target_os = "windows") {
+        // powershell aliases `curl` to `Invoke-WebRequest`
+        "curl.exe"
+    } else {
+        "curl"
+    };
+
+    let mut args = Vec::with_capacity(6);
+    args.extend([
+        "-sSL",
+        "-o",
+        target_dir
+            .as_ref()
+            .as_os_str()
+            .to_str()
+            .expect("target dir should be valid utf-8"),
+        url,
+    ]);
+    let cacert = env::var("CARGO_HTTP_CAINFO").unwrap_or_default();
+    if !cacert.is_empty() {
+        args.extend(["--cacert", &cacert]);
+    }
+
+    let download = std::process::Command::new(curl_bin_name)
+        .args(args)
+        .spawn()
+        .and_then(|mut child| child.wait());
+
+    Ok(download
+        .and_then(|status| {
+            if status.success() {
+                Ok(())
+            } else {
+                Err(std::io::Error::new(
+                    io::ErrorKind::Other,
+                    format!("curl download file exited with error status: {status}"),
+                ))
+            }
+        })
+        .map_err(|error| {
+            if error.kind() == io::ErrorKind::NotFound {
+                io::Error::new(error.kind(), format!("`{curl_bin_name}` command not found"))
+            } else {
+                error
+            }
+        })
+        .map_err(Box::new)?)
+}
 
 pub fn download_and_extract_zip(url: &str, extract_path: &Path) -> Result<(), Box<dyn Error>> {
     // Download the ZIP file
     println!("cargo:warning=Downloading from {}", url);
-    let resp = ureq::get(url).timeout(Duration::from_secs(300)).call()?;
-    let mut content: Vec<u8> = Vec::new();
-    resp.into_reader().read_to_end(&mut content)?;
-
-    // Create a temporary file to store the ZIP
-    let dir = tempdir()?;
-    let zip_path = dir.path().join("libscip.zip");
-
-    let mut temp_file = File::create(&zip_path)?;
-    temp_file.write_all(&content)?;
+    let zip_path = extract_path.join("libscip.zip");
+    download_file_curl(url, zip_path.clone())?;
     let target_dir = PathBuf::from(extract_path);
 
     println!("cargo:warning=Downloaded to {:?}", zip_path);
